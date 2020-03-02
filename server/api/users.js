3dd961af5b93a5036b4b2fb6cde8538c54c5069c
Eugene Sokoletsky <esokoletsky@gmail.com>
2020-03-02 16:38:25 -0500

    testing hello world

--- server/api/users.js
+++ server/api/users.js
@@ -4,13 +4,14 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
+    // const users = await User.findAll({
+    //   // explicitly select only the id and email fields - even though
+    //   // users' passwords are encrypted, it won't help if we just
+    //   // send everything to anyone who asks!
+    //   attributes: ['id', 'email']
+    // })
+    // res.json(users)
+    res.send('Hello World!')
   } catch (err) {
     next(err)
   }
