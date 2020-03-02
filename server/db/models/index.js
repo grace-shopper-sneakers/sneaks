3dfa36725cc6bb0476a47b519d3aeb3cc7a1aef2
Nelson Liu <nelson.ls.liu@gmail.com>
2020-03-02 17:14:54 -0500

    wrote user validation tests

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,16 +1,16 @@
-const User = require('./user');
-const Shoe = require('./shoe');
-const Cart = require('./cart');
-const Order = require('./order');
+const User = require('./user')
+const Shoe = require('./shoe')
+const Cart = require('./cart')
+const Order = require('./order')
 
-User.hasOne(Cart);
-Cart.belongsTo(User);
+User.hasOne(Cart)
+Cart.belongsTo(User)
 
-Cart.hasMany(Shoe, { through: 'CartShoe' });
-Shoe.hasMany(Cart, { through: 'CartShoe' });
+Cart.belongsToMany(Shoe, {through: 'CartShoe'})
+Shoe.belongsToMany(Cart, {through: 'CartShoe'})
 
-Order.belongsTo(User);
-User.hasMany(Order);
+Order.belongsTo(User)
+User.hasMany(Order)
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -26,8 +26,8 @@ User.hasMany(Order);
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-	User,
-	Shoe,
-	Cart,
-	Order
-};
+  User,
+  Shoe,
+  Cart,
+  Order
+}
