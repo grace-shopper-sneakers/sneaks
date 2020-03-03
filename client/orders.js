ce5fc38a48038dc796341743a8aad52fa0ecd8fb
Alexander Wang <alexander.wang22@gmail.com>
2020-03-03 15:47:45 -0500

    updating some orders routes

---
+++ client/orders.js
@@ -0,0 +1,54 @@
+// ACTION TYPES
+const GOT_ORDERS = 'GOT_ORDERS'
+const GOT_SINGLE_ORDER = 'GOT_SINGLE_ORDER'
+const REMOVE_ORDER = 'REMOVE_ORDER'
+const NEW_ORDER = 'NEW_ORDER'
+
+// ACTION CREATORS
+const gotOrders = orders => {
+  return {
+    type: GOT_ORDERS,
+    orders
+  }
+}
+const gotSingleOrder = order => {
+  return {
+    type: GOT_SINGLE_ORDER,
+    order
+  }
+}
+const removeOrder = orderId => {
+  return {
+    type: REMOVE_ORDER,
+    orderId
+  }
+}
+const newOrder = order => {
+  return {
+    type: NEW_ORDER,
+    order
+  }
+}
+//THUNK
+
+// INITIAL STATE
+const initialState = {
+  orders: [],
+  order: {}
+}
+
+//ORDERS REDUCER
+const ordersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GOT_ORDERS:
+      return {...state, orders: action.orders}
+    case GOT_SINGLE_ORDER:
+      return {...state, order: action.order}
+    case REMOVE_ORDER:
+      return state.orders.filter(order => orderId !== action.order.id)
+    case NEW_ORDER:
+      return {...state, orders: [...orders, action.orders]}
+    default:
+      return state
+  }
+}
