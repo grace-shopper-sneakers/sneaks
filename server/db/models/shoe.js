2b898bf5f5e0b984b8b6cb444c3c7773313a9492
Nelson Liu <nelson.ls.liu@gmail.com>
2020-03-03 10:51:04 -0500

    shoe tests completed

--- server/db/models/shoe.js
+++ server/db/models/shoe.js
@@ -1,37 +1,58 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require('sequelize')
+const db = require('../db')
 
 const Shoe = db.define('shoe', {
-	size: {
-		type: Sequelize.INTEGER,
-		allowNull: false
-	},
-	brand: {
-		type: Sequelize.STRING,
-		allowNull: false
-	},
-	color: {
-		type: Sequelize.STRING,
-		allowNUll: false
-	},
-	model: {
-		type: Sequelize.STRING,
-		allowNull: false
-	},
-	name: {
-		type: Sequelize.STRING
-	},
-	description: {
-		type: Sequelize.TEXT
-	},
-	image: {
-		type: Sequelize.STRING,
-		defaultValue: 'no shoe image'
-	},
-	price: {
-		type: Sequelize.FLOAT,
-		allowNull: false
-	}
-});
+  size: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [
+        [
+          '7',
+          '7.5',
+          '8',
+          '8.5',
+          '9',
+          '9.5',
+          '10',
+          '10.5',
+          '11',
+          '11.5',
+          '12',
+          '12.5',
+          '13',
+          '13.5',
+          '14'
+        ]
+      ]
+    }
+  },
+  brand: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  color: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  model: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  image: {
+    type: Sequelize.STRING,
+    defaultValue: 'no shoe image'
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  }
+})
 
-module.exports = Shoe;
+module.exports = Shoe
