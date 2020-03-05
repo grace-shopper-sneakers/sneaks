f3f40c327ebe0a68347b07381cd41ece6fc31ac9
Max Van Buskirk <cmax1018@gmail.com>
2020-03-05 11:01:26 -0500

    refactored models

--- server/db/models/shoe.js
+++ server/db/models/shoe.js
@@ -3,32 +3,27 @@ const db = require('../db')
 
 const Shoe = db.define('shoe', {
   size: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      isIn: [
-        [
-          '7',
-          '7.5',
-          '8',
-          '8.5',
-          '9',
-          '9.5',
-          '10',
-          '10.5',
-          '11',
-          '11.5',
-          '12',
-          '12.5',
-          '13',
-          '13.5',
-          '14'
-        ]
-      ]
-    }
+    type: Sequelize.ENUM(
+      '7',
+      '7.5',
+      '8',
+      '8.5',
+      '9',
+      '9.5',
+      '10',
+      '10.5',
+      '11',
+      '11.5',
+      '12',
+      '12.5',
+      '13',
+      '13.5',
+      '14'
+    ),
+    allowNull: false
   },
   brand: {
-    type: Sequelize.STRING,
+    type: Sequelize.ENUM('Nike', 'Reebok', 'Adidas', 'Converse'),
     allowNull: false
   },
   color: {
@@ -51,7 +46,7 @@ const Shoe = db.define('shoe', {
     defaultValue: 'no shoe image'
   },
   price: {
-    type: Sequelize.FLOAT,
+    type: Sequelize.INTEGER,
     allowNull: false
   }
 })
