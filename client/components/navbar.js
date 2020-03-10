b6105940b468048a84637519d624aae54bf4ea5e
Nelson Liu <nelson.ls.liu@gmail.com>
2020-03-10 11:07:41 -0400

    added item count to cart navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import {connect, useSelector} from 'react-redux'
 import {Link} from 'react-router-dom'
